70f8c9453d58d141c6d6b0d711c84ef5ffca3fe5
Evan <evanmachusak@hotmail.com'sudo git config --global user.password TempoTuesday77sudo git config --global user.email "evanmachusak@hotmail.com>
2017-07-24 16:14:27 -0400

    Build monkeying

---
+++ src/binding.gyp
@@ -0,0 +1,8 @@
+{
+    "targets": [
+        {
+            "target_name": "nodegl",
+            "sources": [ "node.cpp" ]
+        }
+    ]
+}
---
+++ src/nodegl.h
@@ -0,0 +1,7 @@
+#ifndef 
+#define NODEGL_H
+namespace NodeGL {
+    void Method(const FunctionCallbackInfo<Value>& args);
+    void init(Local<Object> exports);
+}
+#endif
