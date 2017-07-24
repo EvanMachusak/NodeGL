d0ab46eec50a74339ad5741f66b7656c485333d2
Evan <evanmachusak@hotmail.com'sudo git config --global user.password TempoTuesday77sudo git config --global user.email "evanmachusak@hotmail.com>
2017-07-23 20:07:20 -0400

    Make file updates

---
+++ src/window.cpp
@@ -0,0 +1,51 @@
+#include "window.h"
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+using namespace std;
+
+int createWindow(int width, int height, const char* title)
+{
+    cout << "running" << endl;
+    if (!glfwInit())
+    {
+        cerr << "failed" << endl;
+        return -1;
+    }
+    else
+    {
+        cout << "glfwInit succeeded" << endl;
+        GLFWwindow *window; // (In the accompanying source code, this variable is global for simplicity)
+        window = glfwCreateWindow(width, height, title, NULL, NULL);
+        if (window == NULL)
+        {
+            cerr << "Failed to initialize window." << endl;
+            glfwTerminate();
+            return -1;
+        }
+        glfwMakeContextCurrent(window); // Initialize GLEW
+        glewExperimental = true;        // Needed in core profile
+        if (glewInit() != GLEW_OK)
+        {
+            fprintf(stderr, "Failed to initialize GLEW\n");
+            return -1;
+        }
+        else
+        {
+            glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
+            do
+            {
+                // Draw nothing, see you in tutorial 2 !
+                // Swap buffers
+                glfwSwapBuffers(window);
+                glfwPollEvents();
+
+            } // Check if the ESC key was pressed or the window was closed
+            while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
+                   glfwWindowShouldClose(window) == 0);
+        }
+    }
+    return 0;
+}
---
+++ src/window.h
@@ -0,0 +1,6 @@
+#ifndef WINDOW_H    // To make sure you don't declare the function more than once by including the header multiple times.
+#define WINDOW_H
+
+int createWindow(int width, int height, const char* title);
+
+#endif
